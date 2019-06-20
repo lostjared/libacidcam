9d9268f70a9c817431b344677b0fe986a52a777a
Jared Bruni <jaredbruni@gmail.com>
2019-06-19 17:24:15 -0700

    changed

--- acidcam-config.cmake
+++ acidcam-config.cmake
@@ -1,13 +1,7 @@
-#get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-#include(${SELF_DIR}/${CMAKE_BUILD_TYPE}/acidcam.cmake)
-
-
-get_filename_component(ACIDCAM_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-set(ACIDCAM_INCLUDE_DIRS "@CONF_INCLUDE_DIRS@")
-include("${ACIDCAM_CMAKE_DIR}/acidcam.cmake")
-
-set(ACIDCAM_LIBRARIES ${CMAKE_INSTALL_PREFIX}/lib/acidcam)
-
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/acidcam/acidcam.cmake)
+#get_filename_component(ACIDCAM_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+#set(ACIDCAM_INCLUDE_DIRS "@CONF_INCLUDE_DIRS@")
 
 
 
