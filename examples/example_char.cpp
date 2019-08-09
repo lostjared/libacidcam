075788c101f18f2e225e7a07b531bd5d65e06d0c
Jared Bruni <jaredbruni@gmail.com>
2019-08-09 15:32:19 -0700

    new test program

---
+++ examples/example_char.cpp
@@ -0,0 +1,63 @@
+#include "ac.h"
+#include<sstream>
+#include<string>
+#include<cstdlib>
+
+void charFrame(int &index, std::string &text, cv::Mat &frame);
+
+int main(int argc, char **argv) {
+    if(argc != 4) {
+        std::cerr << "Error invalid arguments, should be\nexample_char video_in.mov video_out.mov text.txt\n";
+    }
+    std::fstream file;
+    file.open(argv[3], std::ios::in);
+    if(!file.is_open()) {
+        std::cerr << "Error could not open file: " << argv[3] << "\n";
+        exit(EXIT_FAILURE);
+    }
+    std::stringstream stream;
+    stream << file.rdbuf();
+    std::string value = stream.str();
+    cv::VideoCapture cap(argv[1]);
+    if(!cap.isOpened()) {
+        std::cerr << "Error could not open file: " << argv[1] << "\n";
+        exit(EXIT_FAILURE);
+    }
+    ac::fps = cap.get(cv::CAP_PROP_FPS);
+    int aw = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    int ah = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    cv::VideoWriter writer;
+    bool opened = writer.open(argv[2],cv::VideoWriter::fourcc('m','p','4','v'),ac::fps, cv::Size(aw, ah), true);
+    if(opened == false) {
+        std::cerr << "Could not open output file...\n";
+    }
+    std::cout << "Press Q to quit program\n";
+    cv::namedWindow("CharExample");
+    int index = 0;
+    while(1) {
+        cv::Mat frame;
+        cap >> frame;
+        charFrame(index, value, frame);
+        writer.write(frame);
+        cv::imshow("CharExample", frame);
+        int key = cv::waitKey(25);
+        if(key == 28 || key == 'q' || key == 'Q')
+            break;
+    }
+    return 0;
+}
+
+void charFrame(int &index, std::string &text, cv::Mat &frame) {
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            for(int j = 0; j < 3; ++j) {
+                pixel[j] = static_cast<unsigned char>((0.1 * pixel[j])) + (0.9 * (pixel[j]^text[index]));
+                ++index;
+                if(index >= text.length()-1) {
+                    index = 0;
+                }
+            }
+        }
+    }
+}
