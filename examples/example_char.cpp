ae1144f1dec0f95e7f8a0a4448e805b2ffba94b6
Jared Bruni <jaredbruni@gmail.com>
2019-08-09 22:24:10 -0700

    updated example dir

--- examples/example_char.cpp
+++ examples/example_char.cpp
@@ -36,28 +36,41 @@ int main(int argc, char **argv) {
     int index = 0;
     while(1) {
         cv::Mat frame;
-        cap >> frame;
-        charFrame(index, value, frame);
-        writer.write(frame);
-        cv::imshow("CharExample", frame);
-        int key = cv::waitKey(25);
-        if(key == 28 || key == 'q' || key == 'Q')
+        if(cap.read(frame)) {
+            charFrame(index, value, frame);
+            writer.write(frame);
+            int key = cv::waitKey(25);
+            if(key == 28 || key == 'q' || key == 'Q')
+                break;
+            cv::imshow("CharExample", frame);
+        }
+        else
             break;
     }
     return 0;
 }
 
 void charFrame(int &index, std::string &text, cv::Mat &frame) {
+    if(text.length() == 0 || text.length() <= 2) {
+        std::cerr << "Text string is empty.\n";
+        exit(EXIT_FAILURE);
+        return;
+    }
+    if(index+2 > text.length()-1) {
+        index = 0;
+    }
+    char array[3];
+    array[0] = text[index];
+    array[1] = text[index+1];
+    array[2] = text[index+2];
+    index += 3;
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
             cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
             for(int j = 0; j < 3; ++j) {
-                pixel[j] = static_cast<unsigned char>((0.1 * pixel[j])) + (0.9 * (pixel[j]^text[index]));
-                ++index;
-                if(index >= text.length()-1) {
-                    index = 0;
-                }
+                pixel[j] = static_cast<unsigned char>(0.25 * (pixel[j]^array[j]) + (0.25 * array[j]) + (pixel[j] * 0.5));
             }
         }
     }
+    ac::MedianBlendMultiThread(frame);
 }
