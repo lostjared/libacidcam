7ed266d10d7f36956665bf7013d6e4bd522350ff
Jared Bruni <jaredbruni@gmail.com>
2019-10-06 01:31:29 -0700

    updated libav

--- examples/print-codec.cpp
+++ examples/print-codec.cpp
@@ -7,22 +7,18 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 	cv::VideoWriter writer;
-    writer.open(argv[1], cv::VideoWriter::fourcc('a', 'v', 'c', '1'), 30, cv::Size(320, 240), true);
+    writer.open(argv[1],cv::VideoWriter::fourcc('h', 'e', 'v', '1') , 30, cv::Size(320, 240), true);
     if(writer.isOpened()) {
         std::cout << "Opened file...\n";
     }
-    
     cv::Mat mat;
     mat = cv::imread("fred-cover.png");
-    
     if(mat.empty()) {
         std::cerr << "Could not open file...\n";
         exit(EXIT_FAILURE);
     }
-    
     cv::Mat out;
     cv::resize(mat, out, cv::Size(320, 240));
-    
     for(int j = 0; j < 420; ++j)
         writer << out;
     
