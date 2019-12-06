9e31e81194b6147c2e81aff784ae00ec2ed604b9
Jared Bruni <jaredbruni@gmail.com>
2019-12-06 00:29:41 -0800

    new filters; uptodate

--- source/ac.h
+++ source/ac.h
@@ -1906,6 +1906,9 @@ namespace ac {
     void BlockyTrails16(cv::Mat &frame);
     void BlockyTrails32(cv::Mat &frame);
     void BlockyTrails64(cv::Mat &frame);
+    void UseOffRowVert(cv::Mat &frame);
+    void UseOldRowVert(cv::Mat &frame);
+    void UseEveryOtherRowVert(cv::Mat &frame);
     // #NoFilter
     void NoFilter(cv::Mat &frame);
     void Empty(cv::Mat &frame);
