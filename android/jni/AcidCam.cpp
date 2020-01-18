b5bf7fe9cbb643f7ba0f141020185d8c78c3362e
Jared Bruni <jaredbruni@gmail.com>
2020-01-17 20:04:19 -0800

    updated forgot to initalize

--- android/jni/AcidCam.cpp
+++ android/jni/AcidCam.cpp
@@ -11,6 +11,11 @@ using namespace cv;
 JNIEXPORT void JNICALL Java_com_lostsidedead_acidcam_AcidCam_1Filter_Filter
   (JNIEnv *env, jobject pObject, jint filter, jlong frame) {
 	Mat& mRgb = *(Mat*)frame;
+    static int init = 0;
+    if(init == 0) {
+        ac::init();
+        init = 1;
+    }
 	ac::CallFilter(filter, mRgb);
 }
 
