b257c59f8bce442533cb2fad5242fe56216d8f8a
Jared Bruni <jaredbruni@gmail.com>
2020-01-17 21:50:06 -0800

    updated project files

--- android/jni/AcidCam.cpp
+++ android/jni/AcidCam.cpp
@@ -9,14 +9,17 @@ using namespace cv;
 
 
 JNIEXPORT void JNICALL Java_com_lostsidedead_acidcam_AcidCam_1Filter_Filter
-  (JNIEnv *env, jobject pObject, jint filter, jlong frame) {
-	Mat& mRgb = *(Mat*)frame;
+(JNIEnv *env, jobject pObject, jint filter, jlong frame) {
+    Mat& mRgb = *(Mat*)frame;
     static int init = 0;
     if(init == 0) {
         ac::init();
         init = 1;
+        std::cout << "acidcam: initalized...\n";
     }
-	ac::CallFilter(filter, mRgb);
+    std::cout << "frame: width: " << mRgb.cols << "x" << mRgb.rows << "\n";
+    std::cout << "filter: " << filter << "\n";
+    ac::CallFilter(filter, mRgb);
 }
 
 void custom_filter(cv::Mat &frame) {
