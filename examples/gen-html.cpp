584dd49fe26908f9533846e5cff313b28811f27a
Jared Bruni <jaredbruni@gmail.com>
2020-03-26 13:56:35 -0700

    updated source files

--- examples/gen-html.cpp
+++ examples/gen-html.cpp
@@ -10,11 +10,9 @@ void genIndex(int start);
 
 int main(int argc, char **argv) {
     ac::init();
-    ac::setMaxAllocated(3000);
-    
+    ac::setMaxAllocated(2000);
     if(argc == 1)
         genIndex(0);
-    
     if(argc == 3)
         procList(argv[1], atoi(argv[2]), ac::draw_strings);
     
@@ -32,9 +30,9 @@ void procFilter(cv::VideoCapture &cap, std::string file, std::string fname) {
     if(!file_stream.is_open()) {
         std::cerr << "Error could not open file: " << file_name << "\n";
     }
-    file_stream << "<!DOCTYPE html><html><head><title> Acid Cam Filter - " << fname << "</title></head>";
-    file_stream << "<body><h1>" << fname << "</h1><br>Description: <br><br><a href=\"" << gfx_name << "\"><img src=\"" << gfx_name << "\"></a><br><br>";
-    file_stream << "</body></html>";
+    file_stream << "<!DOCTYPE html>\n<html>\n<head><title> Acid Cam Filter - " << fname << "</title></head>\n";
+    file_stream << "<body><h1>" << fname << "</h1><br>Description: <br><br><a href=\"" << gfx_name << "\"><img src=\"" << gfx_name << "\"></a>\n<br><br>";
+    file_stream << "\n</body>\n</html>\n";
     for(int i = 0; i < 8; ++i) {
         cv::Mat frame;
         cap >> frame;
@@ -54,7 +52,7 @@ void genIndex(int start) {
         std::cerr << "Error could not open file.\n";
         exit(EXIT_FAILURE);
     }
-    file << "<!DOCTYPE html><html><head><title>Acid Cam Index</title></head><body><h1>Index</h1><br><br>";
+    file << "<!DOCTYPE html>\n<html>\n<head><title>Acid Cam Index</title></head>\n<body>\n<h1>Index</h1>\n<br><br>";
 
     std::vector<std::string> &names = ac::draw_strings;
     
@@ -63,7 +61,7 @@ void genIndex(int start) {
             file << "<a href=\"" << ac::draw_strings[i] << ".html" << "\">" << ac::draw_strings[i] <<  "</a><br>\n";
         }
     }
-    file << "</body><html>";
+    file << "\n</body>\n<html>";
     std::cout << "generated index..\n";
     return;
 }
