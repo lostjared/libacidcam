2f354c2c1e404ec704906b5824ee967b0622187e
Jared Bruni <jaredbruni@gmail.com>
2020-05-06 13:59:33 -0700

    not working for some reason

---
+++ tests/test-filter-ex.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include"ac.h"
+
+
+int main(int argc, char **argv) {
+
+	ac::init();
+
+	if(argc != 2) {
+		std::cerr << "Error requires file argument..\n";
+		exit(EXIT_FAILURE);
+	}
+	cv::VideoCapture cap(argv[1]);
+	if(!cap.isOpened()) {
+		std::cerr << "Error capture device not opened..\n";
+		exit(EXIT_FAILURE);
+	}
+	for(int i = 0; i < ac::getFilterCount(); ++i) {
+		cv::Mat frame;
+		if(cap.read(frame)) {
+			std::cout << "calling.. " << ac::draw_strings[i] << "\n";
+			ac::CallFilter(i, frame);
+			std::cout << "call complete [" << ac::draw_strings[i] << "]\n";
+		} else { 
+			std::cout << "end of file..\n";
+			exit(EXIT_SUCCESS);
+		}
+	}
+	return 0;
+}
