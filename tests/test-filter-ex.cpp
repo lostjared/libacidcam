7af46caa1d498b45f2b899909031d9783db7be77
Jared Bruni <jaredbruni@gmail.com>
2020-05-30 18:54:57 -0700

    updated tests

--- tests/test-filter-ex.cpp
+++ tests/test-filter-ex.cpp
@@ -3,40 +3,36 @@
 
 
 int main(int argc, char **argv) {
-
-	ac::init();
-
-	if(argc != 2) {
-		std::cerr << "Error requires file argument..\n";
-		exit(EXIT_FAILURE);
-	}
-	cv::VideoCapture cap(argv[1]);
-	if(!cap.isOpened()) {
-		std::cerr << "Error capture device not opened..\n";
-		exit(EXIT_FAILURE);
-	}
-
-	for(int z = 0; z < 2; ++z) {
-
-	for(int i = 0; i < ac::getFilterCount(); ++i) {
-
-	//	if( ac::draw_strings[i].find("Shuffle") != std::string::npos ||  ac::draw_strings[i].find("Rand") != std::string::npos ||  ac::draw_strings[i].find("Frame") != std::string::npos )
-	//		continue;
-
-//		if(ac::draw_strings[i].find("Random") != std::string::npos)
-//			continue;
-
-		cv::Mat frame;
-		if(cap.read(frame)) {
-			std::cout << "calling.. " << ac::draw_strings[i] << "\n";
-			ac::CallFilter(i, frame);
-			std::cout << "call complete [" << ac::draw_strings[i] << "]\n";
-		} else { 
-			std::cout << "end of file..\n";
-			exit(EXIT_SUCCESS);
-		}
-	}
-
-	}
-	return 0;
+    
+    ac::init();
+    
+    if(argc != 2) {
+        std::cerr << "Error requires file argument..\n";
+        exit(EXIT_FAILURE);
+    }
+    cv::VideoCapture cap(argv[1]);
+    if(!cap.isOpened()) {
+        std::cerr << "Error capture device not opened..\n";
+        exit(EXIT_FAILURE);
+    }
+    for(int i = 0; i < ac::getFilterCount(); ++i) {
+        
+        //	if( ac::draw_strings[i].find("Shuffle") != std::string::npos ||  ac::draw_strings[i].find("Rand") != std::string::npos ||  ac::draw_strings[i].find("Frame") != std::string::npos )
+        //		continue;
+        
+        //		if(ac::draw_strings[i].find("Random") != std::string::npos)
+        //			continue;
+        
+        for(int q = 0; q < 30; ++q) {
+            cv::Mat frame;
+            if(cap.read(frame)) {
+                std::cout << "[" << i << "/" << ac::getFilterCount() << "] - calling.. " << ac::draw_strings[i] << "\n";
+                ac::CallFilter(i, frame);
+                std::cout << "call complete [" << ac::draw_strings[i] << "]\n";
+            } else {
+                cap.open(argv[1]);
+            }
+        }
+    }
+    return 0;
 }
