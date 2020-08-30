e88d243637375c6ca3371b6e8210943c6820cb40
Jared Bruni <jaredbruni@gmail.com>
2020-08-29 19:30:37 -0700

    updated test program

--- tests/test-filters.cpp
+++ tests/test-filters.cpp
@@ -1,7 +1,9 @@
 /* Test If Basic Filters are working. */
 
 #include<iostream>
+#include<fstream>
 #include"ac.h"
+#include<chrono>
 
 int main(int argc, char **argv) {
     cv::Mat test_image;
@@ -14,8 +16,12 @@ int main(int argc, char **argv) {
         }
     }
     
-    if(argc == 2)
+    int wait_val = 0;
+    
+    if(argc >= 2)
         test_image = cv::imread(argv[1]);
+    if(argc >= 3)
+        wait_val = atoi(argv[2]);
     
     if(test_image.empty()) {
         std::cerr << "Error image could not be loaded.\n";
@@ -27,20 +33,49 @@ int main(int argc, char **argv) {
     // initialize libacidcam
     ac::fill_filter_map();
     ac::setMaxAllocated(1500);
+    std::fstream file;
+    file.open("output.txt", std::ios::out);
+    if(!file.is_open()) {
+        std::cerr << "Error couldn't open file\n";
+        exit(0);
+    }
     // print out library version
     std::cout << "Library Version: " << ac::getVersion() << "\n";
     for(unsigned int i = 0; i < ac::solo_filter.size()-2; ++i) {
         std::cout << "[" << i << "/" << ac::solo_filter.size() << "] - Testing Filter: " << ac::solo_filter[i] << "\n";
+        bool add = true;
+        
         for(int j = 0; j < 30; ++j) {
             cv::Mat test_image_copy = test_image_sized.clone();
+            std::chrono::time_point<std::chrono::system_clock> now =
+            std::chrono::system_clock::now();
             ac::CallFilter(ac::solo_filter[i], test_image_copy);
             if(j == 29) {
                 //std::ostringstream stream;
                 //stream << "./images/" << ac::solo_filter[i] << ".png";
                 //cv::imwrite(stream.str(), test_image_copy);
             }
+            
+            std::chrono::time_point<std::chrono::system_clock> nowx =
+            std::chrono::system_clock::now();
+            auto m = std::chrono::duration_cast<std::chrono::milliseconds>(nowx-now).count();
+            if(wait_val == 0 || m < wait_val) {
+                add = true;
+            }
+            else {
+                add = false;
+                std::cout << "to slow..\n";
+                break;
+            }
         }
+        
         std::cout << "Passed...\n";
+        if(add == true) {
+            std::cout << "This one is Fast enough\n";
+            file << ac::solo_filter[i] << "\n";
+        } else {
+            std::cout << "This one is not fast enough\n";
+        }
         ac::release_all_objects();
         std::cout << "Cleared Frames...\n";
     }
