e24b5c61f5652b037ff4a7b8e5d7bbf80366fbf3
Jared Bruni <jaredbruni@gmail.com>
2022-05-07 16:29:46 -0700

    added one

--- source/ac.h
+++ source/ac.h
@@ -2502,6 +2502,7 @@ namespace ac {
     void FramePixelsLeftOver(cv::Mat &frame);
     void FramePixelsLeftOverBlend(cv::Mat &frame);
     void FramePixels(cv::Mat &frame);
+    void FrameStopStart(cv::Mat &frame);
     // #NoFilter
     void NoFilter(cv::Mat &frame);
     void Empty(cv::Mat &frame);
